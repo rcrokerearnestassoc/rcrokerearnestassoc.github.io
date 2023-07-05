28d98597dafac909bc4b93bb03afd194bb1a2f52
RileyCrokerEandA <riley.croker@earnestassoc.com>
2023-07-05 08:10:12 -0400

    Removed access code and comments

--- BankListWebApp/index.js
+++ BankListWebApp/index.js
@@ -1,10 +1,7 @@
 // JavaScript source code
 
 function search() {
-  //var cono = document.getElementById("inputField").value;
   var cono = 5001;
-
-  console.log("selected cono: " + cono);
 
   setupDivs();
 
@@ -66,107 +63,10 @@ function setupDivs() {
 }
 
 function searchForBankInfo(cono) {
-  let logindata = {
-    "operator": "RC01",
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    "cono": parseInt(cono)
-  };
-
-  let loginrequestOptions = {
-    method: 'POST',
-    body: logindata
-  };
-
-  let requestdata = {
-    "operator": "RC01",
-    "endpoint": "FetchWhere",
-    "suite": "sxefetch",
-    "method": "Post",
-    "request": {
-      "CompanyNumber": parseInt(cono),
-      "Operator": "RC01",
-      "TableName": "crsb",
-      "WhereClause": `cono = ${cono}`,
-      "BatchSize": 0,
-      "RestartRowID": ""
-    }
-  };
-
-  let requestOptions = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(requestdata)
-  };
-
-  const loginUrl = 'http://localhost:3000/login';
-  const requestUrl = 'http://localhost:3000/request';
   const lambdaUrl = "https://wtdtxdhht2.execute-api.us-east-1.amazonaws.com/default/FetchWhereTest";
-
-  // fetch(loginUrl, loginrequestOptions)
-  //   .then(response => response.json())
-  //   .then(result => {
-  //     console.log(result);
-
-  //     console.log(requestOptions);
-
-  //     fetch(requestUrl, requestOptions)
-  //       .then(response2 => response2.json())
-  //       .then(result2 => fillGrid(result2, cono))
-  //       .catch(error2 => console.log('error', error2));
-  //   })
-  //   .catch(error => console.log('error', error));  
 
   fetch(lambdaUrl)
   .then(response => response.json())
   .then(result => fillGrid(result, cono))
   .catch(error => console.log('error', error));
-
-  // fetch("https://mingle-ionapi.inforcloudsuite.com/EARNEST_DEM/SX/rest/serviceinterface/proxy/FetchWhere", {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     'Authorization': `Bearer ${token}`
-  //   },
-  //   body: JSON.stringify(data),
-  //   mode: "no-cors"
-  // })
-  //   .then(response => {
-  //     if (!response.ok) {
-  //       throw new Error(`HTTP error! Status: ${response.status}`);
-  //     }
-  //     return response.json();
-  //   })
-  //   .then(data => {
-  //     // Handle the response data
-  //     bankInfo = data;
-  //     console.log(data);
-  //   })
-  //   .catch(error => {
-  //     // Handle any errors
-  //     console.error('Error: ', error);
-  //   });
-
-  // try {
-  //   const response = await fetch("https://mingle-ionapi.inforcloudsuite.com/EARNEST_DEM/SX/rest/serviceinterface/proxy/FetchWhere", {
-  //     method: "POST",
-  //     headers: {
-  //       Authentication: 'Bearer ' + token
-  //     },
-  //     body: JSON.stringify(data),
-  //     mode: "no-cors"
-  //   });
-
-  //   //const result = await response.json();
-  //   console.log("Success:", response);
-  //   bankInfo = response;
-  // } catch (error) {
-  //   console.error("Error:", error);
-  //   bankInfo = null;
-  // }
-
-  //return "Bank Info from Company Number: " + bankInfo;
 }
